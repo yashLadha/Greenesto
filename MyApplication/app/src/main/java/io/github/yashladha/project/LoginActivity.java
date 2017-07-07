7ad32ddde413aff7c81bfaad9b9d091f950e17c4
yashLadha <yashladhapankajladha123@gmail.com>
2017-07-07 16:17:31 +0530

    Added demo login ui

--- MyApplication/app/src/main/java/io/github/yashladha/project/LoginActivity.java
+++ MyApplication/app/src/main/java/io/github/yashladha/project/LoginActivity.java
@@ -1,13 +1,34 @@
 package io.github.yashladha.project;
 
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Locale;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email;
+    private Button loginBtn;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        AssetManager am = getApplicationContext().getAssets();
+        Typeface typeface = Typeface.createFromAsset(am, String.format(Locale.US, "fonts/%s", "Junction-regular.otf"));
+
+        email = (EditText) findViewById(R.id.etLoginEmail);
+        password = (EditText) findViewById(R.id.etPassword);
+        loginBtn = (Button) findViewById(R.id.btLogin);
+
+        email.setTypeface(typeface);
+        password.setTypeface(typeface);
+        loginBtn.setTypeface(typeface);
     }
 }
