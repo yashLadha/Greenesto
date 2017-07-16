b769e86e9cd81cc95f8d2a01d63bade5b4e5a7c8
yashLadha <yashladhapankajladha123@gmail.com>
2017-07-16 22:26:26 +0530

    User added to chat(Firebase)

--- MyApplication/app/src/main/java/io/github/yashladha/project/Adapter/PersonsAdapter.java
+++ MyApplication/app/src/main/java/io/github/yashladha/project/Adapter/PersonsAdapter.java
@@ -1,5 +1,6 @@
 package io.github.yashladha.project.Adapter;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -7,6 +8,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
@@ -41,7 +52,7 @@ public class PersonsAdapter extends RecyclerView.Adapter<PersonsAdapter.ViewHold
     return users.size();
   }
 
-  class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+  class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
     TextView email;
     ImageView profileImage;
@@ -55,8 +66,49 @@ public class PersonsAdapter extends RecyclerView.Adapter<PersonsAdapter.ViewHold
 
     @Override
     public void onClick(View v) {
-      TextView temp = (TextView) v.findViewById(R.id.tv_person_email);
-      Log.d(TAG, temp.getText().toString());
+      User userObj = users.get(getAdapterPosition());
+      FirebaseUser curUser = FirebaseAuth.getInstance().getCurrentUser();
+      if (curUser != null)
+        addPersonToChat(userObj.getUid(), curUser.getUid());
+    }
+
+    private void addPersonToChat(final String personUid, String curUserUid) {
+      final DatabaseReference chatEnable = FirebaseDatabase.getInstance().getReference()
+          .child(curUserUid)
+          .child("Chats");
+      chatEnable.addListenerForSingleValueEvent(new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+          chatEnable.child(personUid).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+              if (dataSnapshot.exists()) {
+                Log.d(TAG, "User already added to chat");
+              } else {
+                chatEnable.child(personUid).setValue(personUid)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                      @Override
+                      public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                          Log.d(TAG, "Person added to chat Successfully");
+                        }
+                      }
+                    });
+              }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+              Log.e(TAG, databaseError.getDetails());
+            }
+          });
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+          Log.e(TAG, databaseError.getMessage());
+        }
+      });
     }
   }
 }
