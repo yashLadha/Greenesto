767af04de61f72f0fdc8899d70fc5b756d5626af
yashLadha <yashladhapankajladha123@gmail.com>
2017-07-22 22:31:59 +0530

    Removed .json
    Signed-off-by: yashLadha <yashladhapankajladha123@gmail.com>

--- MyApplication/app/src/main/java/io/github/yashladha/project/Adapter/StorageAdapter.java
+++ MyApplication/app/src/main/java/io/github/yashladha/project/Adapter/StorageAdapter.java
@@ -1,4 +1,28 @@
 package io.github.yashladha.project.Adapter;
 
-public class StorageAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class StorageAdapter extends RecyclerView.Adapter<StorageAdapter.ViewHolder>{
+  @Override
+  public StorageAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    return null;
+  }
+
+  @Override
+  public void onBindViewHolder(StorageAdapter.ViewHolder holder, int position) {
+
+  }
+
+  @Override
+  public int getItemCount() {
+    return 0;
+  }
+
+  public class ViewHolder extends RecyclerView.ViewHolder {
+    public ViewHolder(View itemView) {
+      super(itemView);
+    }
+  }
 }
--- MyApplication/app/src/main/java/io/github/yashladha/project/studentFragments/Storage.java
+++ MyApplication/app/src/main/java/io/github/yashladha/project/studentFragments/Storage.java
@@ -1,24 +1,58 @@
 package io.github.yashladha.project.studentFragments;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+
+import io.github.yashladha.project.Adapter.StorageAdapter;
 import io.github.yashladha.project.R;
+
+import static android.app.Activity.RESULT_OK;
 
 public class Storage extends Fragment {
 
   private String TAG = getClass().getSimpleName();
   private RecyclerView storageList;
+  private StorageAdapter storageAdapter;
+  private static final FirebaseUser CUR_USER = FirebaseAuth.getInstance().getCurrentUser();
+  private static final StorageReference STORAGE = FirebaseStorage.getInstance().getReference()
+      .child(CUR_USER.getUid());
+  private static final DatabaseReference DATABASE = FirebaseDatabase.getInstance().getReference()
+      .child(CUR_USER.getUid()).child("Storage");
+  private FloatingActionButton uploadBtn;
+  private static final int PDF_FIND = 1;
 
   public Storage() {
   }
 
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode == PDF_FIND) {
+      if (resultCode == RESULT_OK) {
+        Uri uri = data.getData();
+        Log.d(TAG, uri.toString());
+        Log.d(TAG, String.valueOf(Uri.fromFile(new File(uri.getPath()))));
+      }
+    }
+  }
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,6 +61,16 @@ public class Storage extends Fragment {
     storageList = (RecyclerView) view.findViewById(R.id.rv_storage_list);
     RecyclerView.LayoutManager lm = new GridLayoutManager(getContext(), 3);
     storageList.setLayoutManager(lm);
+    uploadBtn = (FloatingActionButton) view.findViewById(R.id.fab_storage_upload);
+
+    uploadBtn.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("application/pdf");
+        startActivityForResult(intent, PDF_FIND);
+      }
+    });
     return view;
   }
 
