c990f9f1522b64b463c1584d61d950208de3c6c3
yashLadha <yashladhapankajladha123@gmail.com>
2017-07-22 23:11:07 +0530

    Added file upload and download
    Signed-off-by: yashLadha <yashladhapankajladha123@gmail.com>

--- MyApplication/app/src/main/java/io/github/yashladha/project/studentFragments/Storage.java
+++ MyApplication/app/src/main/java/io/github/yashladha/project/studentFragments/Storage.java
@@ -4,6 +4,7 @@ package io.github.yashladha.project.studentFragments;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
@@ -12,13 +13,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
 
@@ -49,7 +55,27 @@ public class Storage extends Fragment {
       if (resultCode == RESULT_OK) {
         Uri uri = data.getData();
         Log.d(TAG, uri.toString());
-        Log.d(TAG, String.valueOf(Uri.fromFile(new File(uri.getPath()))));
+        StorageReference fileLoc = STORAGE.child(uri.getLastPathSegment());
+        UploadTask uploadTask = fileLoc.putFile(uri);
+
+        uploadTask.addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+          @Override
+          public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+            if (task.isSuccessful()) {
+              Log.d(TAG, "Task is successful");
+              DATABASE.push().setValue(task.getResult().getDownloadUrl().toString())
+                  .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                      if (task.isSuccessful()) {
+                        Toast.makeText(getContext(), "Uploaded successfully", Toast.LENGTH_SHORT)
+                            .show();
+                      }
+                    }
+                  });
+            }
+          }
+        });
       }
     }
   }
